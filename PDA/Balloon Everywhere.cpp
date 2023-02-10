ad7f5397eadacfcbd088cbfd2009a27f93763649
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-02-10 16:35:03 +0530

    Create Balloon Everywhere.cpp

---
+++ PDA/Balloon Everywhere.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int maxInstance(string s){
+         map<char,int>mp;
+
+        for(auto &it:s)
+
+        mp[it]++;
+
+        int ans=min({mp['b'],mp['a'],mp['l']/2,mp['o']/2,mp['n']});
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.maxInstance(s)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
