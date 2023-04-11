c89969c418f9c1da0325838447787eb82a5dbe90
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-04-11 19:10:05 +0530

    Create Maximum Length.cpp

---
+++ PDA/Maximum Length.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    int solve(int a, int b, int c) {
+        // code here
+         int m=max(a,max(b,c));
+        int remain=a+b+c-m;
+        m-=2;
+        if(m<=2*remain){
+            return a+b+c;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        Solution ob;
+        int ans = ob.solve(a, b, c);
+     
+
+        cout <<  ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
