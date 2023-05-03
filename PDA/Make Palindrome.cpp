47052546efa1c3ac3acf282daebb32b2c9e66457
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-05-03 10:56:06 +0530

    Create Make Palindrome.cpp

---
+++ PDA/Make Palindrome.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    bool makePalindrome(int n,vector<string> &arr){
+        // Code here
+        unordered_set<string> st(arr.begin(), arr.end());
+
+    int pairs = 0;
+    
+    for(string tmp : st) {
+        reverse(begin(tmp), end(tmp));
+        if(st.find(tmp) != st.end())
+            ++pairs;
+    }
+    
+    return pairs == n;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        if(ob.makePalindrome(n,arr)) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
