caf5fa543969a0d1fe0bea738d6b6e523ea935d0
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-05-13 12:42:14 +0530

    Create Bit Magic.cpp

---
+++ PDA/Bit Magic.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int bitMagic(int n, vector<int> &arr) {
+        // code here
+         int cnt=0;
+        int i=0;
+        int j=arr.size()-1;
+        
+        while(i<=j){
+            if(arr[i]!=arr[j]){
+                cnt++;
+            }
+            i++;
+            j--;
+        }
+        if(cnt==1)return 1;
+        if(cnt%2!=0){
+            cnt++;
+        }
+        
+        return cnt/2;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        Solution obj;
+        int res = obj.bitMagic(n, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
