59ec69928feee0f6ee7948dad7d8e43fe9cbf51b
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-07-02 11:49:10 +0530

    Create Copy Set Bits in Range.cpp

---
+++ PDA/Copy Set Bits in Range.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution{
+    public:
+    int setSetBit(int x, int y, int l, int r){
+        // code here
+         int mask=((1LL<<r)-1)^((1LL<<(l-1))-1);
+    mask=mask&y;
+    return x|mask;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x, y, l, r;
+        cin>>x>>y>>l>>r;
+        
+        Solution ob;
+        cout<<ob.setSetBit(x, y, l, r)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
