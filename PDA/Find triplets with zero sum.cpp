8a0b53997b756062e0ac44df7d8629c812879900
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-07-08 12:20:39 +0530

    Create Find triplets with zero sum.cpp

---
+++ PDA/Find triplets with zero sum.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+// } Driver Code Ends
+/* You are required to complete the function below
+*  arr[]: input array
+*  n: size of array
+*/
+class Solution{
+  public:
+    //Function to find triplets with zero sum.
+    bool findTriplets(int arr[], int n)
+    { 
+        //Your code here
+        sort(arr,arr+n);
+        for(int i=0;i<n;i++){
+            int a = i+1;
+            int b = n-1;
+            while(a<b){
+                if(arr[a]+arr[b]+arr[i]==0){
+                    return true;
+                }else if(arr[a]+arr[b]+arr[i]<0){
+                    a++;
+                }else{
+                    b--;
+                }
+            }
+            return false;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+	cin>>t;
+	while(t--){
+    	int n;
+    	cin>>n;
+    	int arr[n]={0};
+    	for(int i=0;i<n;i++)
+    		cin>>arr[i];
+    	Solution obj;
+        if(obj.findTriplets(arr, n))
+            cout<<"1"<<endl;
+        else 
+            cout<<"0"<<endl;
+	}
+    return 0;
+}
+// } Driver Code Ends
