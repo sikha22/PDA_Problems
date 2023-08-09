e82f4154aa512599c7d9aef700ef5a797233b6c0
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-08-09 09:47:38 +0530

    Create Largest prime factor.cpp

---
+++ PDA/Largest prime factor.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public: 
+    long long int largestPrimeFactor(int N){
+        // code here
+          long long int maxPrime = -1;
+    
+    while (N % 2 == 0) {
+        maxPrime = 2;
+        N /= 2;
+    }
+
+    for (int i = 3; i <= N; i += 2) {
+        while (N % i == 0) {
+            maxPrime = i;
+            N /= i;
+        }
+    }
+
+    if (N > 2) {
+        maxPrime = N;
+    }
+
+    return maxPrime;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.largestPrimeFactor(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
