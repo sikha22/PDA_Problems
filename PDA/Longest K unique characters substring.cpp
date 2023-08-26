6d150c00c6445823a15784dfe8ea0a537196923e
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-08-26 12:42:13 +0530

    Create Longest K unique characters substring.cpp

---
+++ PDA/Longest K unique characters substring.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int longestKSubstr(string s, int k) {
+    // your code here
+     int i=0;
+    int res=-1;
+    unordered_map<char,int>mp;
+    for(int j=0;j<s.length();j++){
+        mp[s[j]]++;
+        while(mp.size()>k){
+            mp[s[i]]--;
+            if(mp[s[i]]==0){
+                mp.erase(s[i]);
+            }
+            i++;
+        }
+        if(mp.size()==k){
+            res=max(res,j-i+1);
+        }
+    }
+    return res;
+    
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        int k;
+        cin >> k;
+        Solution ob;
+        cout << ob.longestKSubstr(s, k) << endl;
+    }
+}
+
+// } Driver Code Ends
